feedd556c35a206bb04f1e619d17c0859aba676f
Mark Coletti <colettima@ornl.gov>
2021-09-30 17:38:40 -0400

    Added comments and start of test harness in main()

--- parsers/sadlsa_parsers.py
+++ parsers/sadlsa_parsers.py
@@ -1,15 +1,30 @@
+#!/usr/bin/env python3
+"""
+    For importing SAdLSA data into pandas dataframes.
+
+    This is used by another utility for importing into sqlite3 database.
+"""
+
 import pandas as pd
 
+
 def parse_sadlsa_score_file(fn, count=10):
+    """ translate given SAdLSA score file into pandas dataframe
+
+    :param fn: string filename of score file
+    :param count: how many of the top scores we want to get
+    :return: pandas dataframe of
+    """
     import gzip
 
-    columns = ["seqname", "alnlen", "range1", "tmscore1", "range2", "tmscore2", "alnscore", "seq_id", "desc"]
-    df = pd.DataFrame(columns = columns)
+    columns = ["seqname", "alnlen", "range1", "tmscore1", "range2", "tmscore2",
+               "alnscore", "seq_id", "desc"]
+    df = pd.DataFrame(columns=columns)
 
     with gzip.open(fn, mode='rt') as f:
-        lines=f.readlines()
+        lines = f.readlines()
 
-    for line in lines[3:count+3]:
+    for line in lines[3:count + 3]:
         temp = line.strip().split('\t|')
         rec = temp[0].split()
         rec.append(temp[1])
@@ -19,16 +34,25 @@ def parse_sadlsa_score_file(fn, count=10):
 
     return df
 
+
 def parse_sadlsa_aln_file(fn, count=10):
+    """ translate given SAdLSA alignment file into pandas dataframe
+
+    :param fn: string filename of score file
+    :param count: how many of the top scores we want to get
+    :return: pandas dataframe of
+    """
     import gzip
     import re
     import itertools
 
-    prog = re.compile(r'### Alignment (\d+) to: (......) naln=(\d+) score=(\d+\.\d*) tms1=(\d+\.\d*) tms2=(\d+\.\d*) sid=(\d+\.\d*)')
+    prog = re.compile(
+        r'### Alignment (\d+) to: (......) naln=(\d+) score=(\d+\.\d*) tms1=(\d+\.\d*) tms2=(\d+\.\d*) sid=(\d+\.\d*)')
 
-    columns = ["Aln_num", "Prot_ID", "naln", "score", "tms1", "tms2", "sid", "Ind", "Res1", "AA1", "Res2", "AA2", "MeanDist", "Bin", "Prob<3", "Prob<5", "Prob<8"]
-    df = pd.DataFrame(columns = columns)
-
+    columns = ["Aln_num", "Prot_ID", "naln", "score", "tms1", "tms2", "sid",
+               "Ind", "Res1", "AA1", "Res2", "AA2", "MeanDist", "Bin", "Prob<3",
+               "Prob<5", "Prob<8"]
+    df = pd.DataFrame(columns=columns)
 
     with gzip.open(fn) as f:
         lines = f.readlines()
@@ -47,5 +71,8 @@ def parse_sadlsa_aln_file(fn, count=10):
                 series = pd.Series(rec, index=df.columns)
                 df = df.append(series, ignore_index=True)
 
-
     return df
+
+
+if __name__ == '__main__':
+# test harness for these functions
