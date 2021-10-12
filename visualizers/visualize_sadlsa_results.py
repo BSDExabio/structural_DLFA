3b4837e71b7dafb1068eb9ee4c4b3cdeb7829909
Russell Davidson <davidsonrb@ornl.gov>
2021-10-12 03:46:14 -0400

    added scripts to download a desired PDB, edit the b-factor column of said pdb file w/ an array of floats, and then create a VMD visualization state file of this structure.

--- visualizers/visualize_sadlsa_results.py
+++ visualizers/visualize_sadlsa_results.py
@@ -3,16 +3,15 @@
     For pulling information out of SAdLSA pandas dataframes and visualizing results.
 
     Expected functions to be made:
-    1) query_dataframe. Pull important information from the dataframe(s) created by parsing SAdLSA alignment results.
+    1) query_dataframe. Pull important information from the dataframe(s) created by parsing SAdLSA alignment results. (collecting the beta-column values; two dimensional array of nRes_aligned x 2 shape; first column is the resid that had successfully been aligned, second column is the metric of interest)
     2) gather_alignment_structure. Take a PDB ID from the pulled info and search in a structure database (local) or download the relevant structure file from RSCB (remote). Return meta and atom data for later use. 
-    3) gather_alignment_data. Take 
-
+    3) load pdb files and edit the beta column to have the desired values...
+    4) visualize in a molecular visualizer code... I have code to save files for VMD but I don't think this is what we want...
 """
-#from pathlib import Path
-#import pandas as pd
 
 def query_alignment_dataframe():
     """
+    IGNORE THIS FOR NOW...
     Code to pull information from a sqlite database or pandas dataframe or loading a cvs file into memory and reading from that... how should we go about doing this?
     """
 
@@ -20,10 +19,13 @@ def query_alignment_dataframe():
 def grab_structure(pdbid,working_dir='./'):
     """
     Pull the structure from the RCSB database.
-
+    
+    INPUT:
     :param pdbid: string; 4 character long code associated with a structure on the RCSB
-    :param working_dir: string; local or global path string pointing to where the files should be saved; option. NOTE: may not be maintained; hopefully we don't store any files long-term if at all.
-    :return: something... ideally the data structures that have been filled with relevant meta data and structural data. 
+    :param working_dir: string; local or global path string pointing to where the files should be saved; option. TEMPORARY or NOTE: may not be maintained; hopefully we don't store any files long-term if at all.
+    
+    OUTPUT:
+    :return: something probably should be sent back... ideally the data structures that have been filled with relevant meta information and atomic information and coordinates. 
     """
 
     from urllib import request
@@ -54,10 +56,102 @@ def grab_structure(pdbid,working_dir='./'):
             for line in response_list:
                 W.write(line+'\n')
 
+def edit_pdb(atom_data, metric_data, metric_type, string_type='file_path', default_value = -1.00):
+    """
+    Editing the atom data of a pdb-formatted data set; filling the 61-66 columns of the pdb to the float values of the metric_data.
 
+    INPUTS:
+    :param atom_data: kinda up in the air still. either a string of a global or local path to a PDB file OR the string object that contains all of the atomic data. 
+    :param metric_data: a 2d array the contains the metric of interest; first column corresponds to the resids; 2nd column corresponds to the float values associated with the metric of interest. 
+    :param metric_type: a string that acts as a descriptor for the metric; should not contain any spaces. TEMPORARY  
+    
+    OUTPUTS: 
+    A new .pdb file that contains the metric of interest in the b-factor column. 
+    """
 
+    if string_type == 'file_path':
+        import MDAnalysis
+        file_name = atom_data[:-4] + '_' + metric_type + '.pdb'
+        u = MDAnalysis.Universe(atom_data)
+        prot = u.select_atoms('protein')
+        nRes = prot.n_residues
+        for i in range(nRes):
+            if prot.residues[i].resid in metric_data[:,0]:
+                idx = np.argwhere(prot.residues[i].resid == metric_data[:,0])
+                prot.residues[i].tempfactors = float(metric_data[idx,1])   # will be printed with 2 decimal points; 
+            else:
+                prot.residues[i].tempfactors = default_value
+        prot.write(file_name)
 
+    elif string_type == "something_else":
+        print('work with the string object yet to be coded up')
+        break
 
+def create_vmd_vis_state(vis_state_file_name, pdb_file_name, max_color, min_color='lightgray'):
+    """
+    """
+    from scipy import interpolate
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+    from matplotlib import colors
 
+    # ----------------------------------------
+    # SETTING COLORS
+    # ----------------------------------------
+    # white used for residues with no metric value
+    color_rgba = colors.to_rgba('white')
+    color_defs[33] = "color change rgb 33 %.3f %.3f %.3f\n"%(color_rgba[0],color_rgba[1],color_rgba[2])
+    # lightgray used for the minimum metric value
+    min_color_rgba = np.array(colors.to_rgba('lightgray'))
+    # user-set color for the maximum metric value
+    max_color_rgba = np.array(colors.to_rgba(color))
+    # color difference
+    max_min_diff = max_color_rgba - min_color_rgba
 
+    # ----------------------------------------
+    # CREATING/FILLING COLOR DICTIONARIES
+    # ----------------------------------------
+    node_possible_colorids = list(range(34,1057))
+    nNode_colorids = len(node_possible_colorids)
+    cmap_positions = np.linspace(0,1,nNode_colorids)
+    
+    cdict = {'red':[], 'green':[], 'blue':[]}
+    for colorid in node_possible_colorids:
+        idx = colorid - node_possible_colorids[0]
+        thiscolor = np.abs(min_color_rgba + idx * max_min_diff/(nNode_colorids-1))
+        ### VMD Stuff
+        color_defs[colorid] = "color change rgb " + str(colorid) + " " + str(thiscolor[0]) + " " + str(thiscolor[1]) + " " + str(thiscolor[2]) + '\n'
+        ### MATPLOTLIB Stuff
+        cdict['red'].append((cmap_positions[index],thiscolor[0],thiscolor[0]))
+        cdict['green'].append((cmap_positions[index],thiscolor[1],thiscolor[1]))
+        cdict['blue'].append((cmap_positions[index],thiscolor[2],thiscolor[2]))
+
+    with open(vis_state_file_name,'w') as W:
+        ### starting lines
+        W.write('#!/usr/local/bin/vmd\nset viewplist {}\nset fixedlist {}\n\n')
+    
+        ### setting colorids
+        W.write('# setting colorid rgb values\n')
+        W.write(color_defs[33])
+        for i in node_possible_colorids:
+            W.write(color_defs[i])
+    
+        ### prepping the molecule and reps
+        W.write('mol new ' + pdb_file_name + ' type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all\n')
+        W.write('mol delrep 0 top\n')
+        W.write('mol representation NewCartoon 0.160000 50.000000 4.100000 0\n')
+        W.write('mol color Beta\n')
+        W.write('mol selection {all}\n')
+        W.write('mol material AOEdgy\n')
+        W.write('mol addrep top\n')
+        W.write('mol representation Licorice 0.150000 85.000000 85.000000\n')
+        W.write('mol color Name\n')
+        W.write('mol selection { protein }\n')
+        W.write('mol material AOEdgy\n')
+        W.write('mol addrep top\n\n')
+    
+        W.write('### setting viewpoints\n#REPLACE BELOW WITH NEW VIEWPOINTS LINE\nset viewpoints([molinfo top]) {{{1 0 0 0} {0 1 0 0} {0 0 1 0} {0 0 0 1}} {{0 0 0 0} {0 0 0 0} {0 0 0 0} {0 0 0 1}} {{0 0 0 0} {0 0 0 0} {0 0 0 0} {0 0 0 1}} {{1 0 0 0} {0 1 0 0} {0 0 1 0} {0 0 0 1}}}\nlappend viewplist [molinfo top]\nset topmol [molinfo top]\n\nforeach v $viewplist { \n  molinfo $v set {center_matrix rotate_matrix scale_matrix global_matrix} $viewpoints($v)\n}\nforeach v $fixedlist {\n  molinfo $v set fixed 1\n}\nunset viewplist\nunset fixedlist\n')
+    
+    print('Finished creating vis-state file', vis_state_file_name)
+    return
 
