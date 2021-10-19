5a968869e71d7703851bcb601bbe62d3eb7fb09c
Mark Coletti <colettima@ornl.gov>
2021-10-19 13:27:32 -0400

    Add 'inference' feature for those instances where a given locus tag is *similar* to a protein, but isn't a mapping to a protein.

--- parsers/genbank.py
+++ parsers/genbank.py
@@ -16,7 +16,7 @@ def protein_locus_dicts(genbank_file):
     :param genbank_file: is the Genbank file from which we want to read
     :returns: two dicts, protein ID -> locus tag and locus tag -> protein ID
     """
-    records = SeqIO.parse(genbank_file, "genbank")
+    records = list(SeqIO.parse(genbank_file, "genbank"))
 
     locus_to_protein = {}
     protein_to_locus = {}
@@ -46,6 +46,23 @@ def protein_locus_dicts(genbank_file):
                         # the protein ID to locus tag, which we do not want.
                         # protein_to_locus[feature.qualifiers['protein_id'][0]] = \
                         #     old_locus_tag.replace('_','')
+            elif 'inference' in feature.qualifiers:
+                if 'locus_tag' in feature.qualifiers:
+                    for locus_tag in feature.qualifiers['locus_tag']:
+                        locus_to_protein[locus_tag.replace('_','')] = \
+                            feature.qualifiers['inference'][0]
+                        # Apparently this just means this is *similar* to a
+                        # protein, but is not an actual mapping to a protein,
+                        # therefore there is not a valid protein to locust tag
+                        # mapping here.
+                        # protein_to_locus[feature.qualifiers['sequence:RefSeq:'][0]] = \
+                        #     locus_tag.replace('_','')
+
+                if 'old_locus_tag' in feature.qualifiers:
+                    for old_locus_tag in feature.qualifiers['old_locus_tag']:
+                        locus_to_protein[old_locus_tag.replace('_','')] = \
+                            feature.qualifiers['inference'][0]
+
 
     return protein_to_locus, locus_to_protein
 
@@ -58,3 +75,7 @@ if __name__ == '__main__':
 
     assert protein_to_locus['WP_010937312.1'] == 'DVURS00005'
     assert locus_to_protein['DVURS00005'] == 'WP_010937312.1'
+
+    # Catch stupid edge case
+    assert locus_to_protein['DVURS05145'] == 'COORDINATES: similar to AA sequence:RefSeq:WP_011792536.1'
+    assert locus_to_protein['DVU1087'] == 'COORDINATES: similar to AA sequence:RefSeq:WP_011792536.1'
