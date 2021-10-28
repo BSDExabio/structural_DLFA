c06c32678ac270b8031b76a29dea6e05718a4b67
Russell Davidson <davidsonrb@ornl.gov>
2021-10-28 13:01:37 -0400

    cleaning up the parser function and implementing it in the jupyter notebook

--- parsers/af_parsers.py
+++ parsers/af_parsers.py
@@ -6,24 +6,17 @@
 from pathlib import Path
 import pandas as pd
 
-def parse_af_pickle_file(fns,n_models):
+def parse_af_pickle_files(fn_list):
+    """ translate given AlphaFold result files into a pandas dataframe
+    saving the pLDDT results for all user-specified models
+    :param fn_list: list of strings; the global or local paths to pickle files
+    :return: pandas dataframe of models' pLDDT values
     """
-    """
-    import glob
     import pickle
-    
-    # grab appropriate pkl files
-    pickle_files = glob.glob(fns)
-    # sort pkl files
-    pickle_files.sort()
 
-    # check that not too many models are wanted than there are models available
-    if n_models > len(pickle_files):
-        n_models = len(pickle_files)
-    
     # fill in the dictionary with model's associated plddt results
     plddt_dict = {}
-    for i,model_pkl in enumerate(pickle_files[:n_models]):
+    for i, model_pkl in enumerate(fn_list):
         # unpickle a model's result pickle file
         with open(model_pkl,'rb') as file_:
             model_results = pickle.load(file_)
@@ -32,7 +25,7 @@ def parse_af_pickle_file(fns,n_models):
 
     # create the dataframe
     # dataframe columns are zero-indexed numbers that can be used to index the 
-    # sorted glob.glob list of files (either the pkl or model pdb files)
+    # list of files (either the pkl or model pdb files)
     df = pd.DataFrame(plddt_dict)
 
     return df
