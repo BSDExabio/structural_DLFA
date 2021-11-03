145ed395713891882dd1dd8b2a2b6869e62b5ff7
Mark Coletti <colettima@ornl.gov>
2021-11-03 17:35:47 -0400

    PEP8 compliance formatting

--- scripts/mk_info_table.py
+++ scripts/mk_info_table.py
@@ -22,8 +22,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='For creating or updating '
                                                  'the sqlite info table')
     parser.add_argument('database', help='Database to be updated')
-    parser.add_argument('--date', help='Date of this version; defaults to today')
-    parser.add_argument('--version', help='Version of the database; defaults to 0.0')
+    parser.add_argument('--date', help='Date of this version; defaults to '
+                                       'today')
+    parser.add_argument('--version', help='Version of the database; defaults '
+                                          'to 0.0')
 
     args = parser.parse_args()
 
