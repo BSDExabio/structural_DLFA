b9c5c35627b2c8fa9d6b28aa34318fe1bbcf0799
Mark Coletti <colettima@ornl.gov>
2021-11-03 17:31:53 -0400

    Added usage string

--- scripts/mk_info_table.py
+++ scripts/mk_info_table.py
@@ -2,6 +2,18 @@
 """
     Create the sqlite3 info table that describes the version and date
     for the database.
+
+    usage: mk_info_table.py [-h] [--date DATE] [--version VERSION] database
+
+    For creating or updating the sqlite info table
+
+    positional arguments:
+      database           Database to be updated
+
+    optional arguments:
+      -h, --help         show this help message and exit
+      --date DATE        Date of this version; defaults to today
+      --version VERSION  Version of the database; defaults to 0.0
 """
 import argparse
 from database.database import Database
