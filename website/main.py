1de72971565938934d4cd5c4d5d76eb3596f6c1f
Mark Coletti <colettima@ornl.gov>
2021-11-17 18:28:39 -0500

    Start of query for web site

--- website/main.py
+++ website/main.py
@@ -2,9 +2,32 @@
 """
     Main support for the website
 """
-from flask import Flask
+import sqlite3
+import sys
+from pathlib import Path
+
+from database.database import Database
+
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+sqlite3_db = Path('../db/dlfa.db')
+
+@app.route('/', methods = ['POST', 'GET'])
+def query():
+    return render_template('query.html')
+
+@app.route('/list', methods = ['POST'])
+def list():
+    protein_id = request.form['protein']
+
+    rows = Database.query_sadlsa_alignments(protein_id)
+
+
+
+if __name__ == '__main__':
+    if not sqlite3_db.exists():
+        print(f'{sqlite3_db} does not exist; please create and restart')
+        sys.exit(1)
+
+    app.run(debug=True)
