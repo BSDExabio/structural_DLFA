57a024c9f23b1014fc392c1fa1e19110e59847b7
Mark Coletti <colettima@ornl.gov>
2022-06-13 20:42:44 -0400

    Works for the simplest case.

--- parsers/rcsb_scraper.py
+++ parsers/rcsb_scraper.py
@@ -2,26 +2,55 @@
 """
     Scrape RCSB site for chains and enzymes associated with a given protein.
 """
+import re
+from pprint import pprint
+
 import requests
+from bs4 import BeautifulSoup
+
 
 def scrape_rcsb(protein):
     """ Get chains and enzymes for given protein
 
-    Returned dict will have this structure:
-
-    {'protein' : {'chain1' : {'enzyme1' : 'source', 'enzyme2' : 'source'},
-                  'chain2' : {'enzyme3' : 'source'}}}
-
-    You could stack these in a list to later build a pandas dataframe from.
-
     :param protein: for which to get chains and enzymes
-    :return: dict of chains and enzymes, or None if an error
+    :return: chains and their enzymes, or None if none found
     """
-    result = requests.get(f'https://www.rcsb.org/structure/{protein.upper()}')
-    if result.status_code != 200:
+    def extract_chain(table_row):
+        chain = table_row.contents[1].a.text
+
+        enzyme_content = table_row.contents[4].find('a', class_='querySearchLink',
+                                          href=re.compile('rcsb_ec_lineage'))
+
+        if enzyme_content:
+            # FIXME, need to accommodate multiple enzymes
+            enzyme = enzyme_content.text
+        else:
+            return None
+
+        return chain, enzyme
+
+
+    scrape_results = requests.get(f'https://www.rcsb.org/structure/{protein.upper()}')
+    if scrape_results.status_code != 200:
         return None
+
+    result = {'protein' : protein, 'chains' : []}
+
+    soup = BeautifulSoup(scrape_results.text, 'html.parser')
+
+    # Find all the relevant table rows
+    table_rows = soup.find_all(id=re.compile('macromolecule-entityId'))
+
+    for table_row in table_rows:
+        new_chain = extract_chain(table_row)
+        if new_chain: # found a chain with one or more enzymes
+            result['chains'].append(new_chain)
+
+    return result
+
+
 
 if __name__ == '__main__':
     results = scrape_rcsb('6lzm')
 
-    pass
+    pprint(results)
