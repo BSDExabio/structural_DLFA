1d3152abb9b2fbcecebe979c982ce4a8d75a25c4
Mark Coletti <colettima@ornl.gov>
2022-07-06 15:43:50 -0400

    First functional prototype.

--- parsers/rcsb_query.py
+++ parsers/rcsb_query.py
@@ -32,13 +32,34 @@ def query_uniprot(protein, chain):
     :param chain: specific chain in that protein
     :return: Uniprot ID or None if not found
     """
+    uniprot_id = None
+
     r = requests.post('https://data.rcsb.org/graphql',
-                      json={'query' : "query($id: String!){entry(entry_id:$id){exptl{method}}}",
+                      json={'query' : QUERY_STR,
                             'variables' : {'id' : protein.upper()}})
-    pass
+    data = r.json()
+
+    if 'error' in data:
+        # There was something wrong with the query
+        # TODO extract some meaningful error from this for an informative
+        #  exception
+        return None
+
+    chain = chain.upper() # Normalize to chain IDs being upper case
+
+    for entry in data['data']['entries']:
+        for entity in entry['polymer_entities']:
+            if chain in entity['rcsb_polymer_entity_container_identifiers']['asym_ids'] or \
+                chain in entity['rcsb_polymer_entity_container_identifiers']['auth_asym_ids']:
+                uniprot_id = entity['uniprots'][0]['rcsb_id']
+                break
+
+    return uniprot_id
 
 
 if __name__ == '__main__':
     results = query_uniprot('6lzm', 'A')
+    assert results == 'P00720'
+
 
     pass
