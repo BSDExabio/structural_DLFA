d9e8b24d58b522d787e1bc792a8c4baf2095ecae
Mark Coletti <colettima@ornl.gov>
2022-07-21 13:39:53 -0400

    First cut done!

--- scripts/tmalign_2_sqlite3.py
+++ scripts/tmalign_2_sqlite3.py
@@ -16,6 +16,39 @@ from database.database import write_df_to_db
 from parsers.tmalign_parser import parse_tmalign_score_file
 
 
+# sqlite3 table that will contain all the scores
+TMALIGN_SCORES_TABLE = 'tmalign_scores'
+
+
+def add_scores_to_db(tmalign_dir, database):
+    """ Add TMalign score to database for given TMalign data directory
+
+    The data directory should contain two files, one for scores, and the other
+    for alignments.  We want the scores file, which should match the pattern
+    "*_sco.dat8*".
+
+    :param tmalign_dir: output directory containing TMalign scores files
+    :param database: path to sqlite3 database to write the scores table
+    :return: None
+    """
+    # grab score file first
+    score_file = list(sadlsa_dir.glob('*_sco.dat*'))
+    if [] == score_file:
+        logging.critical(
+            f'No score file found in path {sadlsa_dir!s} ... exiting')
+        sys.exit(1)
+
+    # We return the first element of the glob because there should only be one
+    # score file.
+    logging.info(f'Reading {score_file[0]}')
+    tmalign_score_df = parse_tmalign_score_file(str(score_file[0]))
+
+    write_df_to_db(data_frame=tmalign_score_df,
+                   table=TMALIGN_SCORES_TABLE,
+                   database=database)
+
+    logging.info(f'Added {len(tmalign_score_df)} entries to'
+                 f' {database} table {TMALIGN_SCORES_TABLE}.')
 
 
 if __name__ == '__main__':
@@ -36,7 +69,9 @@ if __name__ == '__main__':
     logging.info(f'Ingesting {args.tmalign_dir}')
     start_time = time()
 
+    add_scores_to_db(tmalign_dir, args.database)
 
+    # TODO add alignments to database
 
     logging.info(f'Done ingesting TMalign data into {args.database} in '
                  f'{time() - start_time:0.2f} seconds')
