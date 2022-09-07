98fd9d90a85e3c76895ae46bb5608a5a59b45bc4
[Farhan Quadir] <[fqg7h@mail.missouri.edu]>
2022-09-07 13:41:41 -0500

    time info added to the csv file

--- tmalignM2M/runTMalign1toM.sh
+++ tmalignM2M/runTMalign1toM.sh
@@ -13,27 +13,28 @@ threshold=$(echo $4 | bc)
 echo "INPUT THRESHOLD=$threshold"
 rerundir=$outdir/aligned_pdbs #if maximum TM-score is > than a threshold value then rerun TM-align and save the aligned PDBs here
 rerunflag=0
-header='PDB,RMSD,Length1,TM-score1,Length2,TM-score2'
+header='PDB,RMSD,Length1,TM-score1,Length2,TM-score2,time'
 if [ ! -d $outdir ]; then
 	mkdir -p $outdir
 fi
 
 if [ ! -d $second_pdb ]; then #if second PDB is a PDB file
 	#Extract the RMSD and TM-score portions of the TMalign output
-	IN=$(./TMalign $inp_pdb $second_pdb | grep 'RMSD\|TM-score' | head -3)
+	IN=$(./TMalign $inp_pdb $second_pdb | grep 'RMSD\|TM-score\|CPU time' | head -4)
 	echo "****** $IN"
 	rmsd=$(echo $IN | grep -oP '(?<=RMSD= )[^ ]*' | tr -d ,) 
         tmscore1=$(echo $IN | grep -oP '(?<=TM-score= )[^ ]*' | head -1) 
         length1=$(echo $IN | grep -oP '(?<=LN=)[^ ]*' | tr -d , | head -1)
         tmscore2=$(echo $IN | grep -oP -m 2 '(?<=TM-score= )[^ ]*' | tail -1)
         length2=$(echo $IN | grep -oP -m 2 '(?<=LN=)[^ ]*' | tail -1 | tr -d ,)
+        tim=$(echo $IN | grep -oP -m 2 '(?<=is )[^ ]*' | tail -1 )
         nm=$(basename $second_pdb)
 	name=$(basename $inp_pdb)
 	filename=$outdir/${name%.*}.txt
 	#header='PDB,RMSD,TM-score'
         echo $header > $filename
         echo "${nm%.*},$rmsd,$length1,$tmscore1,$length2,$tmscore2" >> $filename
-	echo "PDB=${nm%.*}\tRMSD=$rmsd\tLength1=$length1\tTM-score1=$tmscore1\tLength2=$length2\tTM-score2=$tmscore2"
+	echo "PDB=${nm%.*}\tRMSD=$rmsd\tLength1=$length1\tTM-score1=$tmscore1\tLength2=$length2\tTM-score2=$tmscore2\ttime=$tim"
 	tm1=$(echo $tmscore1 | bc)
 	tm2=$(echo $tmscore2 | bc)
 	comp=$((`echo "$tm1 > $tm2" | bc`))
@@ -73,17 +74,19 @@ else #If second_pdb is a directory of PDBs
 	#Loop of all the PDB files in the directory of PDB files and run TMalign
 	for pdb2 in $second_pdb/*.pdb; 
 	do
-		IN=$(./TMalign $inp_pdb $pdb2 | grep 'RMSD\|TM-score' | head -3)
-		#echo $IN
+		IN=$(./TMalign $inp_pdb $pdb2 | grep 'RMSD\|TM-score\|CPU time' | head -5)
+		echo "********************8##################### $IN"
+                echo "************************************ END *******************************"
 		rmsd=$(echo $IN | grep -oP '(?<=RMSD= )[^ ]*' | tr -d ,) 
 		tmscore1=$(echo $IN | grep -oP '(?<=TM-score= )[^ ]*' | head -1) 
 		length1=$(echo $IN | grep -oP '(?<=LN=)[^ ]*' | tr -d , | head -1)
 		tmscore2=$(echo $IN | grep -oP -m 2 '(?<=TM-score= )[^ ]*' | tail -1)
 	        length2=$(echo $IN | grep -oP -m 2 '(?<=LN=)[^ ]*' | tail -1 | tr -d ,)
-
+	        tim=$(echo $IN | grep -oP -m 2 '(?<=is )[^ ]*' | tail -1 )
+                echo "******************* TIME = $tim"
 		nm=$(basename $pdb2)
-		echo "${nm%.*},$rmsd,$length1,$tmscore1,$length2,$tmscore2" >> $filename
-		echo "PDB=${nm%.*}\tRMSD=$rmsd\tLength1=$length1\tTM-score1=$tmscore1\tLength2=$length2\tTM-score2=$tmscore2"
+		echo "${nm%.*},$rmsd,$length1,$tmscore1,$length2,$tmscore2,$tim" >> $filename
+		echo "PDB=${nm%.*}\tRMSD=$rmsd\tLength1=$length1\tTM-score1=$tmscore1\tLength2=$length2\tTM-score2=$tmscore2\ttime=$tim"
 		tm1=$(echo $tmscore1 | bc)
 		tm2=$(echo $tmscore2 | bc)
 		comp=$((`echo "$tm1 > $tm2" | bc`))
@@ -102,7 +105,7 @@ else #If second_pdb is a directory of PDBs
 		rerunflag=$((`echo "$maxtmscore >= $threshold" | bc`))
 		
 		if [ $rerunflag -eq 1 ]; then
-			echo "Since Maximum TM-score >= Threshold, rerunning the TMaling to save the aligned PDBs"		
+			echo "Since Maximum TM-score >= $threshold Threshold, rerunning the TMaling to save the aligned PDBs"		
 			if [ ! -d $rerundir ]; then
 				mkdir -p $rerundir
 			fi
