8482aa7f6f365992a08e9ca7b99f5fa7c7cb2072
Russell B. Davidson <rbdchemistry@gmail.com>
2022-09-28 13:00:07 -0400

    numerous small changes and bug fixes while trying to implement on Andes; reaching an impass with the RCSB query step

--- scripts/annotation_pipeline.py
+++ scripts/annotation_pipeline.py
@@ -21,7 +21,7 @@ import uniprot_query
 #######################################
 
 def append_timings(csv_writer, file_object, hostname, worker_id, start_time, stop_time,
-                   query, return_code):
+                   query, task_type, return_code):
     """ append the task timings to the CSV timings file
     :param csv_writer: CSV to which to append timings
     :param file_object: file object associated with the CSV
@@ -30,6 +30,7 @@ def append_timings(csv_writer, file_object, hostname, worker_id, start_time, sto
     :param start_time: start time in *NIX epoch seconds
     :param stop_time: stop time in same units
     :param query: query used as input to a task
+    :param task_type: integer used to denote which step of the workflow has been performed
     :param return_code: result of the task; 1=successful, 0=failure
     """
     csv_writer.writerow({'hostname'   : hostname,
@@ -37,6 +38,7 @@ def append_timings(csv_writer, file_object, hostname, worker_id, start_time, sto
                          'start_time' : start_time,
                          'stop_time'  : stop_time,
                          'query'      : query,
+                         'task_type'  : task_type,
                          'return_code': return_code})
     file_object.flush()
 
@@ -81,17 +83,17 @@ def _parse_alignment_score_file(alignment_results_file, file_type = 'APOC'):
     """
     start_time = time.time()
     worker = get_worker()
-    aln_scores = []
+    aln_scores_df = []
     return_code = 0
 
-    if file_type.upper() = 'APOC':
-        aln_scores = apoc_parsers.parse_apoc_score_file(alignment_results_file)
+    if file_type.upper() == 'APOC':
+        aln_scores_df = apoc_parsers.parse_apoc_score_file(alignment_results_file)
         return_code = 1
-    elif file_type.upper() = 'TMALIGN':
+    elif file_type.upper() == 'TMALIGN':
         pass
-        #aln_scores = apoc_parsers.parse_apoc_score_file(alignment_results_file)
+        #aln_scores_df = apoc_parsers.parse_apoc_score_file(alignment_results_file)
 
-    return 1, aln_scores, platform.node(), worker.id, start_time, time.time(), return_code
+    return 1, aln_scores_df, platform.node(), worker.id, start_time, time.time(), return_code
 
 
 def _query_rcsb(pdbid_chainid):
@@ -104,6 +106,8 @@ def _query_rcsb(pdbid_chainid):
     try:
         uniprotid = rcsb_query.query_uniprot_str(pdbid_chainid)
         return_code = 1
+    except:
+        print(f'failed to pull the uniprot accession id associated with {pdbid_chainid}. oh well...')
 
     return 2, {pdbid_chainid: uniprotid}, platform.node(), worker.id, start_time, time.time(), return_code
 
@@ -118,6 +122,8 @@ def _query_uniprot_flat_file(uniprotid):
     try:
         meta_dict = uniprot_query.request_uniprot_metadata(uniprotid)
         return_code = 1
+    except:
+        print(f'failed to pull {uniprotid} flat file. oh well...')
 
     return 3, {uniprotid: meta_dict}, platform.node(), worker.id, start_time, time.time(), return_code
 
@@ -139,16 +145,19 @@ if __name__ == '__main__':
     #parser.add_argument('--pdbid-to-uniprot-map-pickle', '-map', help='path to a pickle file associated with the mapping between pdbids and uniprot accession ids.')
     args = parser.parse_args()
 
+    output_dir = Path(args.output_dir)
+    tmscore_threshold = float(args.tmscore_threshold)
+    
     # start dask client.
     client = Client(scheduler_file=args.scheduler_file,timeout=5000,name='AlignmentTaskMgr')
 
     # set up the main logger file and list all relevant parameters.
-    main_logger = setup_logger('tskmgr_logger', str(args.output_dir / args.tskmgr_log_file))
+    main_logger = setup_logger('tskmgr_logger', str(output_dir / args.tskmgr_log_file))
     main_logger.info(f'Starting dask pipeline and setting up logging. Time: {time.time()}')
     main_logger.info(f'Scheduler file: {args.scheduler_file}')
     main_logger.info(f'Timing file: {args.timings_file}')
     main_logger.info(f'Alignment score files are listed in {args.input_list_file}')
-    main_logger.info(f'TMscore value cutoff: {args.tmscore_threshold}')
+    main_logger.info(f'TMscore value cutoff: {tmscore_threshold}')
     dask_parameter_string = ''
     for key, value in dask.config.config.items():
         dask_parameter_string += f"'{key}': '{value}'\n"
@@ -157,13 +166,13 @@ if __name__ == '__main__':
     main_logger.info(f'Dask parameters:\n{dask_parameter_string}')
     
     with open(args.input_list_file,'r') as alignment_list_file:
-        alignment_files = alignment_list_file.readlines()
+        alignment_files = [line.strip() for line in alignment_list_file.readlines()]
     main_logger.info(f'{len(alignment_files)} alignment results will be parsed.')
     stem = Path(args.input_list_file).stem
 
     # set up timing log file.
-    timings_file = open( str(args.output_dir / args.timings_file), 'w')
-    timings_csv = csv.DictWriter(timings_file,['hostname','worker_id','start_time','stop_time','query_pdb','task_type','return_code'])
+    timings_file = open( str(output_dir / args.timings_file), 'w')
+    timings_csv = csv.DictWriter(timings_file,['hostname','worker_id','start_time','stop_time','query','task_type','return_code'])
     timings_csv.writeheader()
 
     ### do the thing; only submitting step 1 for now since we know we need to run this set of tasks.
@@ -188,17 +197,17 @@ if __name__ == '__main__':
     # collect futures into a bucket that we will add tasks to
     ac = as_completed(step1_futures)
     for finished_task in ac:
-        task_num, results, platform, workerid, start, stop, return_code = finished_task.result()
+        task_num, results, hostname, workerid, start, stop, return_code = finished_task.result()
         # handling step 1 results:
         if task_num == 1:
-            protID = alignment_pd_df['protein'][0]
-            append_timings(timings_csv,timings_file,platform,workerid,start,stop,protID,return_code)
+            protID = results['protein'][0]
+            append_timings(timings_csv,timings_file,hostname,workerid,start,stop,protID,task_num,return_code)
             main_logger.info(f'Structural alignment file associated with {protID} has been parsed. Return code: {return_code}. Took {stop-start} seconds.')
             if return_code == 1:
                 # removing redundant/unnecessary fields from the pandas dataframe object; store away in the protID_dict dictionary object
-                protID_dict[protID] = alignment_pd_df.drop(labels['protein','Description'],axis=1)
+                protID_dict[protID] = results.drop(labels=['protein','Description'],axis=1)
                 # loop over all pdbid_chainid's marked as "good" hits ('mscore' > tmscore_threshold)
-                for pdbid_chainid in alignment_pd_df.log[alignment_pd_df['mscore'] > args.tmscore_threshold]['tname']:
+                for pdbid_chainid in results.loc[results['mscore'] > tmscore_threshold]['tname']:
                     # only submit a step 2 task if the pdbid_chainid has never been seen before
                     if pdbid_chainid not in pdbid_chainid_list:
                         pdbid_chainid_list.append(pdbid_chainid)
@@ -208,7 +217,7 @@ if __name__ == '__main__':
         elif task_num == 2:
             pdbid_chainid = list(results.keys())[0]
             pdbid_to_uniprot_dict.update(results)
-            append_timings(timings_csv,timings_file,platform,workerid,start,stop,pdbid_chainid,return_code)
+            append_timings(timings_csv,timings_file,hostname,workerid,start,stop,pdbid_chainid,task_num,return_code)
             main_logger.info(f'The UniProt accession ID associated with {pdbid_chainid} has been queried. Return code: {return_code}. Took {stop-start} seconds.')
             # only submit a new step 3 task if the return_code is 1, the uniprot accession id != None or '', and the uniprot aaccession id has not already been seen.
             if return_code == 1 and results[pdbid_chainid] and results[pdbid_chainid] not in uniprotid_list:
@@ -219,19 +228,19 @@ if __name__ == '__main__':
         elif task_num == 3:
             uniprotid = list(results.keys())[0]
             uniprot_metadata_dict.update(results)
-            append_timings(timings_csv,timings_file,platform,workerid,start,stop,uniprotid,return_code)
+            append_timings(timings_csv,timings_file,hostname,workerid,start,stop,uniprotid,task_num,return_code)
             main_logger.info(f'The flat file associated with {uniprotid} has been parsed. Return code: {return_code}. Took {stop-start} seconds.')
     
     # save dictionary of panda dataframes
-    with open( str(args.output_dir / f'{stem}_structural_alignment_results.pkl'), 'w') as out:
+    with open( str(output_dir / f'{stem}_structural_alignment_results.pkl'), 'w') as out:
         pickle.dump(protID_dict,out,protocol=pickle.HIGHEST_PROTOCOL)
 
     # save dictionary of the pdbid_chainid to uniprotid mapping
-    with open( str(args.output_dir / 'pdbid_to_uniprotid_map.pkl'), 'w') as out:
+    with open( str(output_dir / 'pdbid_to_uniprotid_map.pkl'), 'w') as out:
         pickle.dump(pdbid_to_uniprot_dict,out,protocol=pickle.HIGHEST_PROTOCOL)
 
     # save dictionary of uniprot accession id meta data
-    with open( str(args.output_dir / 'uniprot_metadata.pkl'), 'w') as out:
+    with open( str(output_dir / 'uniprot_metadata.pkl'), 'w') as out:
         pickle.dump(uniprot_metadata_dict,out,protocol=pickle.HIGHEST_PROTOCOL)
 
     # gotta finish the annotation pipeline work... matching structure alignment hits to the uniprot metadata... maybe for another script?
