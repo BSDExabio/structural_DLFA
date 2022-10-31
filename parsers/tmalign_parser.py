7d6a714b6555d21ff02e82ccaf7f23671d725d1a
Russell B. Davidson <rbdchemistry@gmail.com>
2022-10-31 13:57:09 -0400

    added the tmalign_parser.py module file; very simple code compared to the apoc_parser.py code because the TMalign results files are csv formatted to begin with.

--- parsers/tmalign_parser.py
+++ parsers/tmalign_parser.py
@@ -1,72 +1,27 @@
 #!/usr/bin/env python3
 """
-    For parsing TMalign-Apoc data.  It expects data like this:
+    For parsing TMalign data.  It expects data like this:
 
-tname  malnlen  mrmsd   mscore  mseqid	Description
-3LFZ_A     125  1.700 0.76562  28.8%	| protein MJ1225; MJ1225, AMPK, AMP, ADP, CBS; HET: ATP, ADP, AMP; 2.2A {Methanocaldococcus jannaschii}
-3KH5_A     125  1.710 0.76354  28.8%	| protein MJ1225; MJ1225, AMPK, AMP, ADP, CBS; HET: ADP, AMP; 2.1A {Methanocaldococcus jannaschii}
+/gpfs/alpine/bif135/proj-shared/rbd_work/databases/PDB70/pdb70_2022_03_19/rbd_work/pdb70_2022_03_19_structures/structure_dir/6CVL_C.pdb,1.84,201,229.0,0.88695,343.0,0.60308,0.88695
+/gpfs/alpine/bif135/proj-shared/rbd_work/databases/PDB70/pdb70_2022_03_19/rbd_work/pdb70_2022_03_19_structures/structure_dir/3TIF_A.pdb,1.95,201,229.0,0.88572,230.0,0.88213,0.88572
+/gpfs/alpine/bif135/proj-shared/rbd_work/databases/PDB70/pdb70_2022_03_19/rbd_work/pdb70_2022_03_19_structures/structure_dir/5LIL_B.pdb,1.96,201,229.0,0.88397,604.0,0.34851,0.88397
+
 """
 from pathlib import Path
 import pandas as pd
 import gzip
 
-def _get_protein(fn):
-    """ return the protein name, which is part of the filename
-        E.g. 'WP_164928147.1_aln.dat' -> 'WP_164928147.1'
-
-        TOTO consolidate this into a utils.py since this function is used in
-        more than one parser.
-    """
-    # Just work backwards to the first '_' in just the file name, and there's
-    # your snipping point
-    name = Path(fn).name  # We do this to snip any leading directory paths
-    return name[:name.rfind('_')]
-
-
 def parse_tmalign_score_file(fn):
     """ Read given TMalign scores file into a pandas dataframe
+    These files are written in csv format to begin with so will
+    be easy to read into a pandas dataframe
 
-    :param fn: of score file
+    :param fn: path to the score file
     :return: pandas dataframe
     """
-    columns = ['protein', 'tname', 'malnlen', 'mrmsd', 'mscore', 'mseqid', 'Description']
-
-    rows = [] # will contain rows of dicts corresponding to score data
-
-    protein = _get_protein(fn)
-
-    if fn[-1] == 'z':
-        # compressed file names should end in 'z'
-        with gzip.open(fn, mode='rt') as f:
-            lines = f.readlines()
-    else:
-        # Someone manually uncompressed the file, probably to look at it
-        with open(fn, mode='rt') as f:
-            lines = f.readlines()
-
-    for line in lines[1:]: # 1: to skip header
-
-        temp = line.strip().split('|')
-        rec = [protein] + temp[0].strip().split() + [temp[1].strip()]   # prepping the list of alignment result elements
-
-        # This is just a fast way of mapping the column names to each
-        # successive list element.  We could have laboriously assigned each
-        # dictionary element, instead, but why do that?  :P
-        temp_dict = {index: value for index, value in zip(columns, rec)}
-
-        # Drop the noisome '%' so that later we can do numeric operations on it
-        temp_dict['mseqid'] = temp_dict['mseqid'][:-1]
-        rows.append(temp_dict.copy())
-
-    df = pd.DataFrame(rows)
-
-    # Converts from objects to strings
-    df = df.convert_dtypes()
-
-    # Convert to numeric those things that are numeric
-    df[[ 'malnlen' , 'mrmsd'  , 'mscore',  'mseqid']] = \
-        df[['malnlen', 'mrmsd', 'mscore', 'mseqid']].apply(pd.to_numeric)
-
+    
+    df = pd.read_csv(fn)
+    
     return df
 
 
@@ -84,6 +39,6 @@ def parse_tmalign_align_file(fn):
 if __name__ == '__main__':
     # test harness for parsers
 
-    df = parse_tmalign_score_file('/Users/may/Projects/data/Alphafold/TMalign-APoc/WP_010939197.1/WP_010939197.1_sco.dat')
+    df = parse_tmalign_score_file('/Users/may/Projects/data/Alphafold/TMalign/WP_010939197.1/TMalign_cp_pdb70_2022_03_19/ranked_alignment_results.dat')
 
     pass
